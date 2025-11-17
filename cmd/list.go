d5dc5c24ea8f1ec590b6211b2d4731d6d76ab75e
Himanshu <himanshuwankhadeh@gmail.com>
2025-11-17 22:32:56 +0530

    list containers cmd added

--- cmd/list.go
+++ cmd/list.go
@@ -1,1 +1,48 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
 package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		ctx := context.Background()
+
+		cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+		if err != nil {
+			panic(err)
+		}
+		defer cli.Close()
+
+		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		if len(containers) == 0 {
+			fmt.Println("No running containers")
+			return
+		}
+
+		for _, c := range containers {
+			fmt.Printf("%s  %-20s  %s\n", c.ID[:12], c.Image, c.Status)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
